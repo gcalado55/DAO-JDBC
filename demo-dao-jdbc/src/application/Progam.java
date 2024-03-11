579bc53d954fe3e2f00683e8226045ed6176742d
Gabriel Calado <gcalado501@gmail.com>
2024-03-11 17:37:02 -0300

    findById implementation

--- demo-dao-jdbc/src/application/Progam.java
+++ demo-dao-jdbc/src/application/Progam.java
@@ -2,18 +2,14 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Progam {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        
-        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.00, obj);
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }

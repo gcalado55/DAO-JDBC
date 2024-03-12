9c0854ced99300b3900d4f95303d0b55c7c920f1
Gabriel Calado <gcalado501@gmail.com>
2024-03-12 17:06:24 -0300

    update implementation

--- demo-dao-jdbc/src/application/Progam.java
+++ demo-dao-jdbc/src/application/Progam.java
@@ -36,5 +36,11 @@ public class Progam {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n=== Test 5: seller update===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update Completed");
+
     }
 }
